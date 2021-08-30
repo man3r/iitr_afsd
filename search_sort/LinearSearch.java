ed59c2a2920e9ddf77b9e055ca9fd98813de3319
Ramesh Mantripragada <rameshma@outlook.com>
2021-08-30 20:35:38 +0530

    updated return type

--- search_sort/LinearSearch.java
+++ search_sort/LinearSearch.java
@@ -3,21 +3,22 @@ package search_sort;
 import java.util.Scanner;
 
 public class LinearSearch {
-    private static boolean linearSearch;
 
-    public boolean linearSearch(int se) {
+    public LinearSearch() {
+    }
+
+    public void linearSearch(int se) {
         // se: search selement
 
         int[] int_array = { 5, 8, 10, 15, 12, 0, 19, 6, 7 };
 
         for (int i = 0; i < int_array.length; i++) {
             if (int_array[i] == se) {
-                System.out.println("Search element " + se + " FOUND at " + i + "th position");
-                return true;
+                System.out.println("Search element " + se + " FOUND at " + i + "th position \n");
+                return;
             }
         }
-        System.out.println("Search element NOT FOUND...");
-        return false;
+        System.out.println("Search element NOT FOUND...\n");
     }
 
     public static void main(String[] args) {
@@ -27,7 +28,7 @@ public class LinearSearch {
         while (se != 0) {
             System.out.println("Enter search element (0 to exit): ");
             se = s.nextInt();
-            boolean b = ls.linearSearch(se);
+            ls.linearSearch(se);
         }
         s.close();
     }
