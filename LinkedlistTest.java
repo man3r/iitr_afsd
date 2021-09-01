41816f87bd1e4502f584aaf1ebbdf803ca7ac215
Ramesh Mantripragada <rameshma@outlook.com>
2021-09-01 21:13:06 +0530

    linked list

---
+++ LinkedlistTest.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class LinkedlistTest {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.add(10);
+        ll.add(20);
+        ll.add(20);
+        ll.add(20);
+        ll.add(20);
+        ll.add(20);
+
+        System.out.println(ll.peek());
+
+        ll.add(3, 100);
+
+        System.out.println(ll);
+
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+        st.
+
+        System.out.println(st);
+
+    }
+
+}
