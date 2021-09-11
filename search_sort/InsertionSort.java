2b114b869b99f1fef52d4e2f86dd5fb7de9cd3f3
Ramesh Mantripragada <rameshma@outlook.com>
2021-09-11 15:46:18 +0530

    ins sort2

--- search_sort/InsertionSort.java
+++ search_sort/InsertionSort.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 public class InsertionSort {
     public static void insertionSort(int array[]) {
         int n = array.length;
-        for (int j = 1; j < n; j++) {
-            int key = array[j];
-            int i = j - 1;
-            while ((i > -1) && (array[i] > key)) {
-                array[i + 1] = array[i];
-                i--;
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while ((j > -1) && (array[j] > key)) {
+                array[j + 1] = array[j];
+                j--;
             }
-            array[i + 1] = key;
+            array[j + 1] = key;
             System.out.println(Arrays.toString(array));
         }
     }
